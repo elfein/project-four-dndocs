2cb1e1a42581798ab420d00267d6a9a5a7a39b6a
elfein <elfein99@gmail.com>
2018-11-06 18:12:25 -0500

    CRUD on items api

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,34 @@
 class Api::ItemsController < ApplicationController
+    def index
+        @items = Character.find(params[:character_id]).items.all
+        render json: @items
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        render json: @item
+    end
+
+    def create
+        @item = Character.find(params[:character_id]).items.create!(item_params)
+        render json: @item
+    end
+    
+    def update
+        @item = Item.find(params[:id])
+        @item.update!(item_params)
+        render json: @item
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+        render status: 200
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :description)
+    end
 end
