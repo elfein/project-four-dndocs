1ddddc570b24bac06dbcbf628b1af4fb35a6ee80
elfein <elfein99@gmail.com>
2018-11-06 17:35:24 -0500

    Refactor api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :accounts do
-      resources :characters do
-        resources :weapons
-        resources :spells
-        resources :items
-        resources :encounters do
-          resources :actions
-        end
-      end
+    resources :accounts, shallow: true do
+      resources :characters
+    end
+    resources :characters, shallow: true do
+      resources :weapons
+      resources :spells
+      resources :items
+      resources :encounters
+    end
+    resources :encounters, shallow: true do
+      resources :actions
     end
   end
 end
