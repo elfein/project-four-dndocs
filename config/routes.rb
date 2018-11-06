27572a04c9112552a514c86935878b8c88497818
elfein <elfein99@gmail.com>
2018-11-06 15:14:55 -0500

    Create api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :accounts do
+      resources :characters do
+        resources :weapons
+        resources :spells
+        resources :items
+        resources :encounters do
+          resources :actions
+        end
+      end
+    end
+  end
 end
