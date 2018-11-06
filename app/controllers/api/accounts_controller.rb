1f9dd496b842d8d9666c3f00cbbd76f7871a4457
elfein <elfein99@gmail.com>
2018-11-06 15:41:54 -0500

    CRUD on Account api

---
+++ app/controllers/api/accounts_controller.rb
@@ -0,0 +1,33 @@
+class Api::AccountsController < ApplicationController
+    def index
+        @accounts = Account.all
+        render json: @accounts
+    end
+
+    def create
+        @account = Account.create!(account_params)
+        render json: @account
+    end
+
+    def show
+        @account = Account.find(params[:id])
+        render json: @account
+    end
+
+    def update
+        @account = Account.find(params[:id])
+        @account.update!(account_params)
+        render json: @account
+    end
+
+    def destroy
+        @account = Account.find(params[:id]).delete
+        render status: :ok
+    end
+
+    private
+
+    def account_params
+        params.require(:account).permit(:name)
+    end
+end
