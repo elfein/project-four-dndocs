47788dfface2e27d17b7dd6b009af93e672d0a01
elfein <elfein99@gmail.com>
2018-11-06 18:30:12 -0500

    CRUD on encounter api

--- app/controllers/api/encounters_controller.rb
+++ app/controllers/api/encounters_controller.rb
@@ -1,2 +1,34 @@
 class Api::EncountersController < ApplicationController
+    def index
+        @encounters = Character.find(params[:character_id]).encounters.all
+        render json: @encounters
+    end
+
+    def show
+        @encounter = Encounter.find(params[:id])
+        render json: @encounter
+    end
+
+    def create
+        @encounter = Character.find(params[:character_id]).encounters.create!(encounter_params)
+        render json: @encounter
+    end
+    
+    def update
+        @encounter = Encounter.find(params[:id])
+        @encounter.update!(encounter_params)
+        render json: @encounter
+    end
+
+    def destroy
+        @encounter = Encounter.find(params[:id])
+        @encounter.destroy
+        render status: 200
+    end
+
+    private
+
+    def encounter_params
+        params.require(:encounter).permit(:encounter_type)
+    end
 end
