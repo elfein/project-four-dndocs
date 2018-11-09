ec7de759a06e3dbc2d9801d4ed3653506e32fece
elfein <elfein99@gmail.com>
2018-11-09 10:56:53 -0500

    Enable toggle to character items on info

--- client/src/components/CharacterDataContainer.jsx
+++ client/src/components/CharacterDataContainer.jsx
@@ -1,22 +1,48 @@
 import React, { Component } from 'react'
 import CharacterStatList from './CharacterStatList';
+import CharacterItemContainer from './CharacterItemContainer';
 
 export default class CharacterDataContainer extends Component {
-    state = {
-        showStats: true,
-        showItems: false,
-        showSpells: false
-    }
+  state = {
+    showStats: true,
+    showItems: false,
+    showSpells: false
+  }
+
+  showSkills = () => {
+    this.setState({ showStats: true, showItems: false, showSpells: false })
+  }
+
+  showItems = () => {
+    this.setState({ showStats: false, showItems: true, showSpells: false })
+  }
+
+  showSpells = () => {
+    this.setState({ showStats: false, showItems: false, showSpells: true })
+  }
+
   render() {
-      const character = this.props.character
+    const character = this.props.character
     return (
       <div>
         <div className="selectors">
-            <button>Skills</button>
+          <button onClick={this.showSkills}>Skills</button>
+          <button onClick={this.showItems}>Inventory</button>
+          <button onClick={this.showSpells}>Spells</button>
         </div>
-        <CharacterStatList 
+
+        {this.state.showStats ?
+          <CharacterStatList
+            getCharacter={this.props.getCharacter}
+            character={character} /> : null}
+
+        {this.state.showItems ? 
+        <CharacterItemContainer
         getCharacter={this.props.getCharacter}
-        character={character} />
+        character={character}
+        /> : null}
+
+        {this.state.showSpells ? 'spells' : null}
       </div>
     )
   }
---
+++ client/src/components/CharacterItemContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CharacterItemContainer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Weapons</p>
+        <p>Items</p>
+      </div>
+    )
+  }
+}
